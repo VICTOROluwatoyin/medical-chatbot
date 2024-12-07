5bca88a57934180e9cc433ad8974713109756a9a
MasterKim <victoregbinade30@gmail.com>
2024-12-07 21:36:56 +0100

    New Edit made

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Master kim',
+    author_email= 'victoregbinade30@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
